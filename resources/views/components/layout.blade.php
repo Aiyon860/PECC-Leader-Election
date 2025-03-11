90ce0bc5b94b96b7ad6d00b7580376df1fc9d11c
danielaiyon <yonyondap23@gmail.com>
2025-03-11 22:33:09 +0700

    feat: menambahkan file view, components, dan static asset yang diperlukan

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PECC Leader Election 2025</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="h-full">
+    <aside>
+        <nav>
+
+        </nav>
+    </aside>
+    <main>
+
+    </main>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,63 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+})->name("login")->middleware("guest");
+
+Route::get("/dashboard", function () {
+  return view("admin.dashboard");
+})->middleware("auth");
+
+Route::get("/result", function () {
+  return view("admin.result");
+})->middleware("auth");
+
+Route::get("/candidates", function () {
+  return view("admin.candidates.display-candidates");
+})->middleware("auth");
+
+Route::get("/candidates/add-candidate", function () {
+  return view("admin.candidates.add-edit-candidate-form");
+})->middleware("auth");
+
+Route::get("/candidates/edit-candidate", function () {
+  return view("admin.candidates.add-edit-candidate-form");
+})->middleware("auth");
+
+Route::get("/voters", function () {
+  return view("admin.voters.display-voters");
+})->middleware("auth");
+
+Route::get("/voters/add-voter", function () {
+  return view("admin.voters.add-voter-form");
+})->middleware("auth");
+
+Route::get("/voters/import-voters", function () {
+  return view("admin.voters.import-voters");
+})->middleware("auth");
+
+Route::get("/voters/remove-voters", function () {
+  return view("admin.voters.remove-voters");
+})->middleware("auth");
+
+Route::get("/vote", function () {
+  return view("user.vote");
+})->middleware("auth");
+
+Route::get("/thank-you", function () {
+  return view("user.thank-you");
+})->middleware("auth");
+
+Route::middleware("auth")->group(function () {
+  Route::get("/dashboard");
+  Route::get("/result");
+  Route::get("/candidates");
+  Route::get("/candidates/add-candidate");
+  Route::get("/candidates/edit-candidate");
+  Route::get("/voters");
+  Route::get("/voters/add-voter");
+  Route::get("/voters/import-voters");
+  Route::get("/voters/remove-voters");
+  Route::get("/vote");
+  Route::get("/thank-you");
 });
